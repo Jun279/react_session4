aefb9993841a10a0146f2cfbe095f5883a1111da
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-09-22 17:12:48 +0900

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TodoList from "../components/list/todo-list";
+
+const Home = () => {
+    return(
+        <>
+            <TodoList/>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/util/todo-dataset.jsx
@@ -0,0 +1,32 @@
+const TodoDataset = [
+    {
+        "id": 1,
+        "title": "",
+        "isDone": false,
+        "image": "/images/1.png"
+    },
+    {
+        "id": 2,
+        "title": "",
+        "isDone": false,
+        "image": "/images/2.png"
+    },
+    {
+        "id": 3,
+        "title": "",
+        "isDone": false,
+        "image": "/images/3.png"
+    },
+    {
+        "id": 4,
+        "title": "",
+        "isDone": false,
+        "image": "/images/4.png"
+    },
+    {
+        "id": 5,
+        "title": "",
+        "isDone": false,
+        "image": "/images/5.png"
+    },
+]
